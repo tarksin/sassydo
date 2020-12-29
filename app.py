503d851a55db4ba4aa6b8756698dbf8b789137ab
Tark Sinilind <tark@maxxima.net>
2020-12-29 15:11:11 -0600

    necklaces.html and images

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, static_url_path='/static')
+
+app.config['DEBUG']= False
+app.config['SECRET_KEY'] = 'ddd91417-11fa-40b5-98c1-fde58db3086d'
 
 @app.route('/')
 def index():
-    return '<h3>sassydo index()</h3>'
-
+    return  render_template('necklaces.html')
 
 @app.route('/necklaces')
 def necklaces():
-    return '<h3>sassydo necklaces()</h3>'
+    return  render_template('necklaces.html')
